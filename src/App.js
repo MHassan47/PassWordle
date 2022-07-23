8f2d18e8613c7c2cd0914de00035240785578572
Mohamed <mhassankd35@gmail.com>
2022-07-23 18:48:59 -0500

    imports necessary files, minor change to context API value

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
+import { boardDefault } from "../src/Words";
 import Board from "./components/Board/Board";
 import Keyboard from "./components/Keyboard/Keyboard";
 import { createContext } from "react";
+import { useState } from "react";
 
 export const AppContext = createContext();
+
 function App() {
   const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
-      <AppContext.Provider value={(board, setBoard)}>
+      <AppContext.Provider value={{ board, setBoard }}>
         <Board />
         {/* <Keyboard /> */}
       </AppContext.Provider>
