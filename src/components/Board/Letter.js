0ad4d7995360147892b5037d12e4eb007c0180a5
Mohamed <mhassankd35@gmail.com>
2022-07-23 18:49:53 -0500

    refactors jsx to render dynamically based on input

--- src/components/Board/Letter.js
+++ src/components/Board/Letter.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import "./Letter.css";
 function Letter({ letterPosition, attemptValue }) {
-  return <div className="letter">Letter</div>;
+  const { board } = useContext(AppContext);
+  const letter = board[attemptValue][letterPosition];
+  return <div className="letter">{letter}</div>;
 }
 
 export default Letter;
