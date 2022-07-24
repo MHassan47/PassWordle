0bcd342285bfbaa8a09864ee96d55928a014c0b9
Mohamed <mhassankd35@gmail.com>
2022-07-23 22:21:50 -0500

    refactors logic to be placed in child key component

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Keyboard.css";
+
+import Key from "./Key";
 const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
@@ -9,30 +11,18 @@ function Keyboard() {
     <div className="keyboard">
       <div className="line_one">
         {keys1.map((key) => {
-          return (
-            <div key={key} className="key">
-              {key}
-            </div>
-          );
+          return <Key key={key} keyValue={key} />;
         })}
       </div>
       <div className="line_two">
         {keys2.map((key) => {
-          return (
-            <div key={key} className="key">
-              {key}
-            </div>
-          );
+          return <Key key={key} keyValue={key} />;
         })}
       </div>
       <div className="line_three">
         <div className="big key">ENTER</div>
         {keys3.map((key) => {
-          return (
-            <div key={key} className="key">
-              {key}
-            </div>
-          );
+          return <Key key={key} keyValue={key} />;
         })}
         <div className="big key">DELETE</div>
       </div>
