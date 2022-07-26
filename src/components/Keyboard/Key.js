04e57ddb8f752a8f9906849aa84457145b3abe8b
Mohamed <mhassankd35@gmail.com>
2022-07-26 09:48:39 -0500

    adds ENTER and DELETE key logic

--- src/components/Keyboard/Key.js
+++ src/components/Keyboard/Key.js
@@ -2,21 +2,42 @@ import React from "react";
 import "./Keyboard.css";
 import { useContext } from "react";
 import { AppContext } from "../../App";
-function Key({ keyValue }) {
+function Key({ keyValue, bigKey }) {
   const { board, setBoard, currentAttempt, setCurrentAttempt } =
     useContext(AppContext);
 
   const selectLetter = () => {
-    const newBoard = [...board];
-    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyValue;
-    setBoard(newBoard);
-    setCurrentAttempt({
-      ...currentAttempt,
-      letterPosition: currentAttempt.letterPosition + 1,
-    });
+    console.log("clicked");
+    if (keyValue === "ENTER") {
+      console.log("ENTER");
+      if (currentAttempt.letterPosition !== 5) return;
+      setCurrentAttempt({
+        ...currentAttempt,
+        attempt: currentAttempt.attempt + 1,
+        letterPosition: 0,
+      });
+    } else if (keyValue === "DELETE") {
+      if (currentAttempt.letterPosition === 0) return;
+      setCurrentAttempt({
+        ...currentAttempt,
+        attempt: currentAttempt.attempt,
+        letterPosition: currentAttempt.letterPosition - 1,
+      });
+    } else {
+      if (currentAttempt.letterPosition > 4) return;
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition] =
+        keyValue;
+      setBoard(newBoard);
+      setCurrentAttempt({
+        ...currentAttempt,
+        letterPosition: currentAttempt.letterPosition + 1,
+      });
+    }
   };
+  console.log(currentAttempt);
   return (
-    <div className="key" onClick={selectLetter}>
+    <div className="key" onClick={selectLetter} id={bigKey && "big"}>
       {keyValue}
     </div>
   );
