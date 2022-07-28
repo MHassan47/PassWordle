cb521466caf01cf899f934cacb6eef94d055b7a3
Mohamed <mhassankd35@gmail.com>
2022-07-28 06:46:14 -0500

    adds logic for keyboard events

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useContext, useCallback, useEffect } from "react";
 import "./Keyboard.css";
 
 import Key from "./Key";
+import { AppContext } from "../../App";
 const keys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function Keyboard() {
+  const { onSelectLetter, onEnter, onDelete } = useContext(AppContext);
+  const handleKeyboard = useCallback((event) => {
+    if (event.key === "Enter") {
+      onEnter();
+    } else if (event.key === "Backspace") {
+      onDelete();
+    } else {
+      keys.forEach((key) => {
+        if (event.key === key) {
+          onSelectLetter(key);
+        }
+      });
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
     <div className="keyboard">
       {keys.map((key) => {
