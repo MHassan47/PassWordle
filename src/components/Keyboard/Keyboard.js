dad0a002cad184a930118a82a1ee53bb998c7b62
Mohamed <mhassankd35@gmail.com>
2022-07-27 23:31:17 -0500

    refactors keyboard to be design responsive

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -7,11 +7,11 @@ const keys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 function Keyboard() {
   return (
     <div className="keyboard">
-      <Key keyValue={"ENTER"} bigKey />
       {keys.map((key) => {
         return <Key key={key} keyValue={key} />;
       })}
 
+      <Key keyValue={"ENTER"} bigKey />
       <Key keyValue={"DELETE"} bigKey />
     </div>
   );
