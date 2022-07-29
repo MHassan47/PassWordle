37f7e46ea2de10b84ec5d91559476d578e7d05a5
Mohamed <mhassankd35@gmail.com>
2022-07-29 05:34:20 -0500

    adds useEffect that houses logic for setting disabled number state

--- src/components/Board/Letter.js
+++ src/components/Board/Letter.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import "./Letter.css";
 function Letter({ letterPosition, attemptValue }) {
-  const { board, correctPassword, currentAttempt } = useContext(AppContext);
+  const {
+    board,
+    correctPassword,
+    currentAttempt,
+    disabledLetters,
+    setDisabledLetters,
+  } = useContext(AppContext);
   const number = board[attemptValue][letterPosition];
 
   const correct = correctPassword[letterPosition] === number;
@@ -12,6 +18,13 @@ function Letter({ letterPosition, attemptValue }) {
   const colorID =
     currentAttempt.attempt > attemptValue &&
     (correct ? "correct" : wrongSpot ? "close" : "incorrect");
+
+  useEffect(() => {
+    if (number !== "" && !correct && !wrongSpot) {
+      console.log(number);
+      setDisabledLetters((prev) => [...prev, number]);
+    }
+  }, [currentAttempt.attempt]);
   return (
     <div className="letter" id={colorID}>
       {number}
